8f8a0efe501c09b2c2ec4b446220733f8ed55402
Dmitrii Sibert <dimaaasik.s@gmail.com>
2018-07-10 19:42:34 +0600

    Updated makefile

--- src/core/ICardDeck.h
+++ src/core/ICardDeck.h
@@ -1,17 +1,17 @@
-#ifndef CARD_DECK_INTERFACE_H_ 
-#define CARD_DECK_INTERFACE_H_ 
+#ifndef CARD_DECK_INTERFACE_H_
+#define CARD_DECK_INTERFACE_H_
 
-#include <string> 
+#include <string>
 
-class ICardDeck 
+class ICardDeck
 {
 
-public: 
-	virtual void Shuffle() = 0; 
-	virtual void Reverse() = 0; 
-	virtual std::string GetName() = 0; 
-	virtual ~ICardDeck() = 0; 
-}; 
+public:
+	virtual void Shuffle() = 0;
+	virtual void Reverse() = 0;
+	virtual std::string GetName() = 0;
+	virtual ~ICardDeck() = 0;
+};
 
 
 #endif /** CARD_DECK_INTERFACE_H_ */
